ead78e723e5dd0ad402dfe9bfb3982638c69b6d9
krola1 <kwl082024@gmail.com>
2024-11-25 10:26:49 +0100

    bang

--- main.js
+++ main.js
@@ -9,3 +9,31 @@ const chuCkButton = document.getElementById("chuck-button");
 const randUrl = "https://official-joke-api.appspot.com/random_joke";
 const dadUrl = "https://icanhazdadjoke.com/";
 const chuckUrl = "https://api.chucknorris.io/jokes/random";
+
+async function getRandJoke() {
+  const response = await fetch(randUrl);
+  const data = await response.json();
+  setup.textContent = data.setup;
+  punchline.textContent = data.punchline;
+}
+async function getDadJoke() {
+  const response = await fetch(dadUrl, {
+    headers: { Accept: "application/json" },
+  });
+  const data = await response.json();
+
+  setup.textContent = data.joke;
+  punchline.textContent = "";
+}
+const getChuckJoke = async () => {
+  const response = await fetch(chuckUrl);
+  const data = await response.json();
+
+  setup.textContent = data.value;
+  punchline.textContent = "";
+};
+getDadJoke();
+
+randButton.addEventListener("click", getRandJoke);
+dadButton.addEventListener("click", getDadJoke);
+chuCkButton.addEventListener("click", getChuckJoke);
